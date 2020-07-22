d19c9326ba0e6e953f11a3120dc9a24b76ba9ea3
Vasile Cojusco <codersinhoods@MacBook-Pro-2.broadband>
2020-07-22 09:12:25 +0100

    update

---
+++ 31-diy-array-iterators-exercise/index.js
@@ -0,0 +1,118 @@
+/**
+ * Exercise #1
+ *
+ * forEach(array, callback)
+ *
+ * The callback is invoked for every
+ * element in the array and is
+ * passed each element and the index.
+ *
+ */
+
+/**
+ * Exercise #2
+ *
+ * map(array, callback)
+ *
+ * The callback is invoked for every
+ * element in the array and is passed
+ * each element and the index. A new
+ * array is returned which contains
+ * whatever was returned from each
+ * time the callback was invoked.
+ *
+ */
+
+/**
+ * Exercise #3
+ *
+ * filter(array, callback)
+ *
+ * The callback is invoked for every
+ * element in the array and is passed
+ * each element and the index. A new
+ * array is returned which contains
+ * _only_ the elements for which the
+ * callback returned a truthy value.
+ *
+ */
+
+/**
+ * Exercise #4
+ *
+ * find(array, callback)
+ *
+ * The callback is invoked for every
+ * element in the array and is passed
+ * each element and the index. The
+ * function returns the _first element_
+ * for which the callback returns a
+ * truthy value.
+ *
+ */
+
+/**
+ * Exercise #5
+ *
+ * findIndex(array, callback)
+ *
+ * The callback is invoked for every
+ * element in the array and is passed
+ * each element and the index. The
+ * function returns the _index of
+ * the first element_ for which the
+ * callback returns a truthy value.
+ *
+ */
+
+/**
+ * Exercise #6
+ *
+ * every(array, callback)
+ *
+ * The callback is invoked for every
+ * element in the array and is passed
+ * each element and the index. The
+ * function returns a boolean value
+ * representing if every time the
+ * callback was invoked it returned
+ * a truthy value.
+ *
+ */
+
+/**
+ * Exercise #7
+ *
+ * some(array, callback)
+ *
+ * The callback is invoked for every
+ * element in the array and is passed
+ * each element and the index. The
+ * function returns a boolean value
+ * representing if _any_ time the
+ * callback was invoked it returned
+ * a truthy value.
+ *
+ */
+
+/**
+ * Exercise #8
+ *
+ * reduce(array, callback, initialValue)
+ *
+ * Reduce is often used for combining
+ * values together.
+ *
+ * `reduce` should take 3 arguments:
+ * `array`, `callback` and `initialValue`.
+ *
+ * The callback is invoked for every
+ * element in the array and is passed
+ * **the current cumulative value**,
+ * each element and the index. Whatever
+ * the callback returns is the new
+ * cumulative value. The function
+ * should return the final cumulative
+ * value.
+ *
+ */
